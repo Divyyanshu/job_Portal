840ff5e197d047459d7b9f75019a0ea7cb9615c1
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-12-23 13:10:31 +0530

    api integration

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className='flex justify-center items-center h-screen'>
-     <p className='text-5xl font-bold text-slate-500'>HomePage's</p>
+     <p className='text-5xl font-bold text-slate-500'>HomePage</p>
     </div>
   )
 }
--- src/pages/JobsPages.jsx
+++ src/pages/JobsPages.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
-
+import React, { useEffect, useReducer } from 'react'
+import axios from 'axios'
+import { RotateLoader } from 'react-spinners';
 const JobsPages = () => {
+
+  const initialState = {
+    loading: false,
+    data: null,
+    error: null
+  }
+  const reducer = (prevState, action) => {
+    if (action.type === 'Loading') {
+      return { ...prevState, loading: true }
+    } else if (action.type === 'Success') {
+      return { ...prevState, loading: false, data: action.payload }
+    } else if (action.type === 'Error') {
+      return { ...prevState, loading: false, error: action.payload }
+    }
+  }
+  const [currentState, dispatch] = useReducer(reducer, initialState)
+  // fetch api using useEffect()
+  useEffect(() => {
+    async function fetchData() {
+      dispatch({ type: 'Loading' })
+      try {
+        const data = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        dispatch({ type: 'Success', payload: data.data })
+      }
+      catch (error) {
+        dispatch({ type: 'Error', payload: error.message })
+      }
+    }
+    fetchData();
+  }, [])
+
   return (
-    <div className='flex justify-center items-center h-screen'>
-     <p className='text-5xl font-bold text-slate-500'>Job's</p>
+    <div className='flex justify-center m-auto items-center h-screen text-xl'>/
+      {currentState.loading ? (
+        <RotateLoader />
+      ) : currentState.data ? (
+        <p>{JSON.stringify(currentState.data)}</p>
+      ) : (
+        <p className='text-xl font-bold text-red-500'>{currentState.error}</p>
+      )}
     </div>
+    // shot circuit method
+    // <div className='flex flex-col justify-center items-center h-screen'>
+    //   {currentState.loading && (
+    //     <RotateLoader />
+    //   )}
+    //   <p>{JSON.stringify(currentState.data)}</p>
+    //   <p>{currentState.error}</p>
+    // </div>
   )
 }
 
